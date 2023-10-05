52e9685206388e4606234efe76cbf02ebc25345a
getlarge <ed@getlarge.eu>
2023-10-05 15:34:21 +0200

    test: duplicate and patch original aedes tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,28 @@
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended'],
   root: true,
+  overrides: [
+    {
+      files: ['*.ts'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      rules: {},
+    },
+    {
+      files: ['*.js'],
+      parserOptions: {
+        ecmaVersion: 'latest',
+      },
+      env: {
+        es6: true,
+        node: true,
+      },
+      rules: {
+        "no-unused-vars": "off",
+      },
+    },
+  ],
 }
